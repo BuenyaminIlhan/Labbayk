9364f175158ea115e1f19ac396ba5de18f5c6355
Büny <ilhan.buenyamin@gmail.com>
2025-03-19 09:15:46 +0100

    Feat: Implement search field and improve UI
    -   Adds `SearchField` composable for searching through Quran chapters.
    -   Adds `ScrollAwareSearchField` composable to show or hide the search field based on scroll position.
    -   Updates `QuranListScreen` to include and manage the new search field.
    -    Updates `SearchField` to use `TextField` with leading/trailing icons.
    -    Updates `ScrollAwareSearchField` to use a `Column` to manage scroll-dependent visibility and manage the search field.
    - Updates some UI elements to white color.
    - Improve design of search bar.

--- app/src/main/java/de/bueny/labbayk/ui/screens/QuranListScreen.kt
+++ app/src/main/java/de/bueny/labbayk/ui/screens/QuranListScreen.kt
@@ -7,21 +7,29 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -60,6 +69,59 @@ fun QuranListScreen(
             }
         }
     }
+}
+
+@OptIn(UnstableApi::class)
+@kotlin.OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchField(
+    onTitleChange: (String) -> Unit,
+) {
+    var localTitle by remember { mutableStateOf("") }
+
+    TextField(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .padding(bottom = 6.dp)
+            .fillMaxWidth()
+            .shadow(2.dp, shape = RoundedCornerShape(12.dp))
+            .background(Color.White, shape = RoundedCornerShape(12.dp)),
+        value = localTitle,
+        onValueChange = { newText ->
+            localTitle = newText
+            onTitleChange(newText)
+        },
+        singleLine = true,
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "Search",
+                tint = Color.Gray
+            )
+        },
+        trailingIcon = {
+            if (localTitle.isNotEmpty()) {
+                IconButton(onClick = { localTitle = "" }) {
+                    Icon(Icons.Default.Clear, contentDescription = "Clear", tint = Color.Gray)
+                }
+            }
+        },
+        label = { Text("Suchen", color = Color.Gray) },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                Log.d("TopBar", "onDone: $localTitle")
+            }
+        ),
+        shape = RoundedCornerShape(12.dp),
+        colors = TextFieldDefaults.colors(
+            cursorColor = Color.Black,
+            focusedIndicatorColor = Color.Transparent, // Keine Unterstreichung
+            unfocusedIndicatorColor = Color.Transparent // Keine Unterstreichung
+        )
+    )
 }
 
 @OptIn(UnstableApi::class)
@@ -109,44 +171,80 @@ fun QuranListItem(
     }
 }
 
+
 @OptIn(UnstableApi::class)
+@kotlin.OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchField(
-    onTitleChange: (String) -> Unit,
+fun ScrollAwareSearchField(
+    onTitleChange: (String) -> Unit
 ) {
     var localTitle by remember { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+    var isSearchVisible by remember { mutableStateOf(false) }
+    var lastScrollPosition by remember { mutableStateOf(0) }
 
-    OutlinedTextField(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth(),
-        value = localTitle,
-        onValueChange = { newText ->
-            localTitle = newText
-            onTitleChange(newText)
-        },
-        singleLine = true,
-        leadingIcon = {
-            Icon(
-                Icons.Default.Search,
-                contentDescription = "Search"
+    LaunchedEffect(scrollState.value) {
+        isSearchVisible = scrollState.value < lastScrollPosition
+        lastScrollPosition = scrollState.value
+    }
+
+    Column {
+        if (isSearchVisible) {
+            TextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .padding(bottom = 6.dp)
+                    .fillMaxWidth()
+                    .shadow(2.dp, shape = RoundedCornerShape(12.dp))
+                    .background(Color.White, shape = RoundedCornerShape(12.dp)),
+                value = localTitle,
+                onValueChange = { newText ->
+                    localTitle = newText
+                    onTitleChange(newText)
+                },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = "Search",
+                        tint = Color.Gray
+                    )
+                },
+                trailingIcon = {
+                    if (localTitle.isNotEmpty()) {
+                        IconButton(onClick = { localTitle = "" }) {
+                            Icon(Icons.Default.Clear, contentDescription = "Clear", tint = Color.Gray)
+                        }
+                    }
+                },
+                label = { Text("Suchen", color = Color.Gray) },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        Log.d("TopBar", "onDone: $localTitle")
+                    }
+                ),
+                shape = RoundedCornerShape(12.dp),
+                colors = TextFieldDefaults.colors(
+                    cursorColor = Color.Black,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
             )
-        },
-        trailingIcon = {
-            if (localTitle.isNotEmpty()) {
-                IconButton(onClick = { localTitle = "" }) {
-                    Icon(Icons.Default.Clear, contentDescription = Icons.Default.Clear.name)
-                }
+        }
+
+        // Dummy-Scrollable-Content zum Testen
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
+            repeat(50) {
+                Text("Item $it", modifier = Modifier.padding(16.dp))
             }
-        },
-        label = { Text("Kapitelnummer") },
-        keyboardOptions = KeyboardOptions.Default.copy(
-            imeAction = ImeAction.Done
-        ),
-        keyboardActions = KeyboardActions(
-            onDone = {
-                Log.d("TopBar", "onDone: $localTitle")
-            }
-        )
-    )
+        }
+    }
 }
+
