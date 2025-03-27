08879c765cbd42cb7087887c52277ab4716ae484
Bünyamin Ilhan <>
2025-03-27 09:18:19 +0300

    Refactor: Enhance navigation and improve top bar behavior
    - Adds back button for `ChapterDetailRoute` and `FavoriteDetailScreen`.
    - Modifies the bottom navigation bar to only show when NOT on `ChapterDetailRoute` or `FavoriteDetailScreen`.
    - Updates `App.kt` to include `showBackButtonOnDetail` and `showBackButtonOnFavorite` variables to control back button visibility.
    - Updates `App.kt` to use these variables to display `CustomTopBar` correctly.
    - Updates `App.kt` to update `BottomNavigationBar` visible only if `showBackButtonOnDetail` and `showBackButtonOnFavorite` are false.
    - Updates the deployment target selection in `deploymentTargetSelector.xml`

--- app/src/main/java/de/bueny/labbayk/ui/App.kt
+++ app/src/main/java/de/bueny/labbayk/ui/App.kt
@@ -4,11 +4,9 @@ import android.annotation.SuppressLint
 import androidx.annotation.OptIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,28 +14,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import de.bueny.labbayk.ui.components.BottomNavigationBar
-import de.bueny.labbayk.ui.components.TopBar
 import de.bueny.labbayk.ui.navigation.AppNavHost
 import de.bueny.labbayk.ui.navigation.ChapterDetailRoute
+import de.bueny.labbayk.ui.navigation.FavoriteDetailScreen
 import de.bueny.labbayk.ui.navigation.NavigationItem
-import de.bueny.labbayk.ui.screens.SearchField
 
 @SuppressLint("RestrictedApi")
 @OptIn(UnstableApi::class)
@@ -53,19 +46,20 @@ fun App() {
     val currentBackStack by navHostController.currentBackStackEntryAsState()
     val currentDestination = currentBackStack?.destination
 
-    val showBottomBar = !(currentDestination?.route?.contains(ChapterDetailRoute.route) ?: false)
+    val showBackButtonOnDetail = currentDestination?.hierarchy?.any { it.route == ChapterDetailRoute.route } == true
+    val showBackButtonOnFavorite = currentDestination?.hierarchy?.any { it.route == FavoriteDetailScreen.route } == true
 
     Scaffold(
         containerColor = primaryBackgroundColor,
         topBar = {
-            if (true) {
+            if (showBackButtonOnDetail || showBackButtonOnFavorite) {
                 CustomTopBar(
                     primaryBackgroundColor, quranViewModel,navHostController
                 )
             }
         },
         bottomBar = {
-            if (showBottomBar) {
+            if (!showBackButtonOnDetail && !showBackButtonOnFavorite) {
                 BottomNavigationBar(
                     navHostController,
                     selectedNavItem,
